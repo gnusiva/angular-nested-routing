42b9af3f5bef9e1391411b3468a0040d33796a4e
Siva <sivam1@netapp.com>
2021-01-22 08:39:25 +0530

    second level routing done

--- src/app/modules/html/html-routing.module.ts
+++ src/app/modules/html/html-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeadingComponent } from './heading/heading.component';
 import { HtmlComponent } from './html.component';
+import { ImageComponent } from './image/image.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
 
 const routes: Routes = [
-  { path: '', component: HtmlComponent }
+  { path: '', component: HtmlComponent, children: [
+    { path: 'image', component: ImageComponent },
+    { path: 'paragraph', component: ParagraphComponent },
+    { path: 'heading', component: HeadingComponent }
+  ]},
 ];
 
 @NgModule({
--- src/app/modules/html/html.module.ts
+++ src/app/modules/html/html.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HtmlRoutingModule } from './html-routing.module';
 import { HtmlComponent } from './html.component';
+import { ImageComponent } from './image/image.component';
+import { ParagraphComponent } from './paragraph/paragraph.component';
+import { HeadingComponent } from './heading/heading.component';
 
 
 @NgModule({
-  declarations: [HtmlComponent],
+  declarations: [HtmlComponent, ImageComponent, ParagraphComponent, HeadingComponent],
   imports: [
     CommonModule,
     HtmlRoutingModule
