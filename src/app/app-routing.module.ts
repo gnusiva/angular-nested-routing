bf15e093559080d60a8fa44a640d19c937e3d7e1
Siva <sivam1@netapp.com>
2021-01-22 08:06:49 +0530

    feature 2 further update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // na thaan add panna poren... feature 2
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
