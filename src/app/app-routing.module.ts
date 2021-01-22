9b293e5ecd316e4431ba948bd65a55e4f7a98822
Siva <sivam1@netapp.com>
2021-01-22 08:05:26 +0530

    planning to add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // inga thaan feature1 ku code varapoguthu...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
