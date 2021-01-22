6dbeada28483e4cfad4bfd2c40c4738679195968
Siva <sivam1@netapp.com>
2021-01-22 07:47:21 +0530

    first commit

---
+++ src/app/modules/html/html.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HtmlRoutingModule } from './html-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HtmlRoutingModule
+  ]
+})
+export class HtmlModule { }
---
+++ src/app/modules/javascript/javascript.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { JavascriptRoutingModule } from './javascript-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    JavascriptRoutingModule
+  ]
+})
+export class JavascriptModule { }
